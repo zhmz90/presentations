6b5dc61839a81cf4dcf5c3adf1b4c394c53002aa
Lanting Guo <guolanting@haplox.com>
2015-09-02 10:54:17 +0800

    clean the older .md

--- src/build.sh
+++ src/build.sh
@@ -1,6 +1,6 @@
 
 pandoc present_julia.md -t beamer -V theme:Warsaw  -o present_julia.pdf
 
-pandoc present_julia.md -t beamer -V theme:Warsaw    -s -o present_julia.tex
+#pandoc present_julia.md -t beamer -V theme:Warsaw    -s -o present_julia.tex
 
 
---
+++ src/presentation.md
@@ -0,0 +1,5 @@
+% Introduction to julia
+% Lanting Guo
+% \today
+
+
